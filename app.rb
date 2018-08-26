3970548b378b2377f2d3a6fba39a98c3bc073e94
Yurina Shin <yurinas@gmail.com>
2018-08-26 02:39:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,6 @@ class App < Sinatra::Base
     erb :user_input
   end
 
-  post '/' do
-    a = PigLatinizer.new
-    @converted_text = a.piglatinize(params[:user_phrase])
-    erb :piglatinize
-  end
 
   post '/piglatinize' do
     a = PigLatinizer.new
